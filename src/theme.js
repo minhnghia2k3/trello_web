dda295511d8c6071b084f6aa2ad1de688aee13f7
minhnghia2k3 <BrozennVN@gmail.com>
2023-07-06 16:43:00 +0700

    Config dark/light mode Part 1

--- src/theme.js
+++ src/theme.js
@@ -1,24 +1,25 @@
-import { createTheme } from '@mui/material/styles'
-import { red } from '@mui/material/colors'
+// import { createTheme } from '@mui/material/styles'
+import { experimental_extendTheme as extendTheme } from '@mui/material/styles'
+import { pink } from '@mui/material/colors'
 
 // Create a theme instance.
-const theme = createTheme({
-    palette: {
-        mode: 'dark',
-        primary: {
-            main: '#556cd6'
+const theme = extendTheme({
+    colorSchemes: {
+        light: {
+            palette: {
+                primary: {
+                    main: pink[600]
+                }
+            }
         },
-        secondary: {
-            main: '#19857b'
-        },
-        error: {
-            main: red.A400
-        },
-        text: {
-            secondary: red[500]
+        dark: {
+            palette: {
+                primary: {
+                    main: pink[400]
+                }
+            }
         }
     }
-
 })
 
 export default theme
