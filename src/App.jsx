6616a6b44f06f85182b07346bf3e53949a0de0bc
minhnghia2k3 <BrozennVN@gmail.com>
2023-07-10 22:29:43 +0700

    Create layout for trello app

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,4 @@
-// import * as React from 'react'
-import Button from '@mui/material/Button'
-import { AccessAlarm, ThreeDRotation } from '@mui/icons-material'
-import DeleteIcon from '@mui/icons-material/Delete'
-import HomeIcon from '@mui/icons-material/Home'
-import { pink } from '@mui/material/colors'
-import Typography from '@mui/material/Typography'
 import { useColorScheme } from '@mui/material/styles'
-import useMediaQuery from '@mui/material/useMediaQuery'
 
 import InputLabel from '@mui/material/InputLabel'
 import MenuItem from '@mui/material/MenuItem'
@@ -18,6 +10,7 @@ import Box from '@mui/material/Box'
 import LightModeIcon from '@mui/icons-material/LightMode'
 import DarkModeIcon from '@mui/icons-material/DarkMode'
 import SettingsBrightnessIcon from '@mui/icons-material/SettingsBrightness'
+import Container from '@mui/material/Container'
 
 function ModeSelect() {
   const { mode, setMode } = useColorScheme()
@@ -57,51 +50,44 @@ function ModeSelect() {
   )
 }
 
-function ModeToggle() {
-  const { mode, setMode } = useColorScheme()
-  return (
-    <Button
-      onClick={() => { setMode(mode === 'light' ? 'dark' : 'light') }}
-    >
-      {mode === 'light' ? 'Turn dark' : 'Turn light'}
-    </Button>
-  )
-}
+
 
 function App() {
-  // const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)') // get system theme => if dark => return true
-  // const prefersLightMode = useMediaQuery('(prefers-color-scheme: light)')
 
 
   return (
-    <>
-      <ModeSelect />
-      <hr />
-      <ModeToggle />
+    <Container maxWidth='false' disableGutters='true' sx={{ height: '100vh' }}>
+      <Box sx={{
+        width: '100%',
+        height: (theme) => theme.trello.appBarHeight,
+        backgroundColor: 'primary.light',
+        display: 'flex',
+        alignItems: 'center'
+      }}>
+        <ModeSelect />
+      </Box>
 
-      <hr />
-      <div>Le minh nghia</div>
+      <Box sx={{
+        width: '100%',
+        height: (theme) => theme.trello.boardBarHeight,
+        backgroundColor: 'primary.dark',
+        display: 'flex',
+        alignItems: 'center'
+      }}
+      >
+        App Bar
+      </Box>
+      <Box sx={{
+        display: 'flex',
+        alignItems: 'center',
+        backgroundColor: 'primary.main',
+        height: (theme) => `calc(100vh - ${theme.trello.appBarHeight} - ${theme.trello.boardBarHeight})`
+      }}
+      >
+        Board Bar
+      </Box>
 
-      <Typography variant="h1" color="text.secondary">
-        h1. Heading
-      </Typography>
-
-      <Button variant="text">Text</Button>
-      <Button variant="contained">Contained</Button>
-      <Button variant="outlined">Outlined</Button>
-      <br />
-      <AccessAlarm />
-      <ThreeDRotation />
-      <svg data-testid="DeleteIcon"><DeleteIcon /></svg>
-      <HomeIcon />
-      <HomeIcon color="primary" />
-      <HomeIcon color="secondary" />
-      <HomeIcon color="success" />
-      <HomeIcon color="action" />
-      <HomeIcon color="disabled" />
-      <HomeIcon sx={{ color: pink[500] }} />
-
-    </>
+    </Container>
   )
 }
 
